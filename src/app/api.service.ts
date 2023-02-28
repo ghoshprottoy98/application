c3f29532b8ba4ff4311945947e067733e532a648
Prottoy Ghosh <ghoshprottoy98@gmail.com>
2023-02-28 11:40:35 +0600

    master

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ProductModel } from './models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string)
+  {
+    return this.http.post('login', {username, password});
+  }
+
+  getProducts(): Observable<ProductModel[]>{
+    return this.http.get<ProductModel[]>('products', {
+      headers: {},
+    });
+  }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
